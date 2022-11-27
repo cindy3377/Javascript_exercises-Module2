32ff53e195ac0f2cba61129cb3b3f08eb0c984ff
cindy3377 <92247502+cindy3377@users.noreply.github.com>
2022-11-27 15:49:09 +0200

    upload ex5

---
+++ Exercises5/app5.js
@@ -0,0 +1,20 @@
+document.write("Exercise 5<br><br>")
+
+let num = parseInt(prompt("Enter the number:"));
+let nums = [num];
+let isDuplicated = false
+
+while (!isDuplicated) {
+    var ul = document.createElement("ul");
+    num = parseInt(prompt("Enter the number:"));
+    if (nums.indexOf(num) == -1) {
+        nums.push(num);
+        nums.sort((a, b) => a-b)
+    }
+    else {
+        document.write(`The number ${num} has already been entered.`)
+        isDuplicated = true
+        ul.innerHTML += `<li>${nums}</li>`;
+        document.body.appendChild(ul);
+    }
+}
