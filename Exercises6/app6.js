91aa3b4f136900dcabbccbf7536329898cd0dcd9
cindy3377 <92247502+cindy3377@users.noreply.github.com>
2022-11-27 20:25:52 +0200

    update ex6

--- Exercises6/app6.js
+++ Exercises6/app6.js
@@ -1,7 +1,6 @@
 document.write("Exercise 6<br><br>")
 
-/*Write a function that returns a random dice roll between 1 and 6. The function should not have any parameters. Write a main program that rolls the dice until the result is 6. The main program should print out the result of each roll in an unordered list (<ul>). (2p)
-*/
+
 function randomDice () {
     let results = [];
     let randomResult = 0
